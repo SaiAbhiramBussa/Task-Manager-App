750af5cc965500c35bdc87d910e7bddd1d24b491
SaiAbhiramBussa <bussasaiabhiram@gmail.com>
2026-02-24 01:52:53 +0530

    fix: manual migration for task priority and category

---
+++ tasks/migrations/0002_task_category_task_priority.py
@@ -0,0 +1,20 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tasks', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='task',
+            name='category',
+            field=models.CharField(default='Personal', max_length=50),
+        ),
+        migrations.AddField(
+            model_name='task',
+            name='priority',
+            field=models.CharField(choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], default='medium', max_length=10),
+        ),
+    ]
